ee244791cce45d0db808c1c8a264e0b13e7bcd5b
This-my-life <1731875634@qq.com>
2019-11-08 16:50:08 +0800

    第一次

---
+++ GIS_ArcEngine_fisrtapp/Form1.cs
@@ -0,0 +1,77 @@
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.Geodatabase;
+using GIS_ArcEngine_fisrtapp.function;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GIS_ArcEngine_fisrtapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //axMapControl1.LoadMxFile(@"D:\luwei\test2\八维建筑.mxd");
+            //test
+        }
+
+        private void mxdToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "shp文件|*.shp|mxd文件|*.mxd|ext files (*.txt)|*.txt|All files(*.*)|*>**";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                if (open.FileName != null || open.FileName != "")
+                {
+                    axMapControl1.LoadMxFile(open.FileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("请选择文件");
+                return;
+            }
+        }
+
+        private void shpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GeoUtil util = new GeoUtil(axMapControl1);
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                util.loadShapefile(dialog.FileName);
+            }
+        }
+
+        int i = 0;
+        DataTable dt = new DataTable();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Column
+            dt.Columns.Add(new DataColumn("字段"+i, typeof(string)));
+            // Row
+            string [] rowStr = { "你好" + i };
+            dt.Rows.Add(rowStr);
+
+            dataGridView1.DataSource = dt;
+            i++;
+        }
+
+        private void mySQLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //122333
+        }
+    }
+}
---
+++ GIS_ArcEngine_fisrtapp/function/GeoUtil.cs
@@ -0,0 +1,73 @@
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.Geodatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GIS_ArcEngine_fisrtapp.function
+{
+    class GeoUtil
+    {
+        private AxMapControl mapControl;
+        public GeoUtil()
+        {
+            //
+        }
+        public GeoUtil(AxMapControl mapControl)
+        {
+            this.mapControl = mapControl;
+        }
+        public void loadShapefile(string shpFileName)
+        {
+            // 文件格式判断
+            if(shpFileName.Substring(shpFileName.LastIndexOf(".")) != ".shp")
+            {
+                MessageBox.Show("请选择shp文件");
+                return;
+            }
+            // 文件打开
+            string filePath = "";
+            string fileName = "";
+            int index = shpFileName.LastIndexOf("\\");  // 14
+            Console.WriteLine(index);
+            if(index > 0)
+            {
+                filePath = shpFileName.Substring(0, index);
+                string temp = shpFileName.Substring(index + 1);
+                fileName = temp.Substring(0, temp.LastIndexOf("."));
+            }
+
+            IWorkspaceFactory pWorkspaceFactory;
+            IFeatureWorkspace pFeatureWorkspace;
+            IFeatureClass pFeatureClass;
+            IFeatureLayer pFeatureLayer;
+            pWorkspaceFactory = new ShapefileWorkspaceFactory();
+            IWorkspace pWorkspace = pWorkspaceFactory.OpenFromFile(filePath, 0);
+            pFeatureWorkspace = pWorkspace as IFeatureWorkspace;
+            // 将shp数据读入内存 --- >> pFeatureClass
+            pFeatureClass = pFeatureWorkspace.OpenFeatureClass(fileName);  // 练习ArcMap
+
+            //pFeatureLayer = new FeatureLayer();
+            //pFeatureLayer.FeatureClass = pFeatureClass;
+            //pFeatureLayer.Name = "region";
+            //axMapControl1.AddLayer(pFeatureLayer);
+            //axMapControl1.Refresh();
+
+            // 用于图形渲染   ---->   pFeatureLayer
+            pFeatureLayer = new FeatureLayer();
+            pFeatureLayer.FeatureClass = pFeatureClass;
+            IDataset pDataset = pFeatureClass as IDataset;
+            pFeatureLayer.Name = pDataset.Name;  // set
+            //pFeatureLayer.Name = "afdsafdsafs";  // set
+            //string s = pFeatureLayer.Name; // get
+            ILayer pLayer = pFeatureLayer as ILayer;
+            this.mapControl.Map.AddLayer(pLayer);
+            this.mapControl.Refresh();
+        }
+    }
+}
